fbdbf84be663488df64351f635958a315d9004d2
Karol P <karol@przybylski>
2022-05-14 20:30:00 +0200

    Working passing data to line chart template. Enabled chart autoscaling. Removed dead code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Markup, render_template
 import sqlite3
+from collections import OrderedDict
 
 app = Flask(__name__)
 
@@ -10,37 +11,24 @@ def get_db_connection():
     return conn
 
 
-labels = [
-    'JAN', 'FEB', 'MAR', 'APR',
-    'MAY', 'JUN', 'JUL', 'AUG',
-    'SEP', 'OCT', 'NOV', 'DEC'
-]
-
-values = [
-    967.67, 1190.89, 1079.75, 1349.19,
-    2328.91, 2504.28, 2873.83, 4764.87,
-    4349.29, 6458.30, 9907, 16297
-]
-
-colors = [
-    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
-    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
-    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
-
-
 @app.route('/')
 def index():
     conn = get_db_connection()
     posts = conn.execute('SELECT * FROM temperature').fetchall()
     conn.close()
-    return dict(posts)
+    return OrderedDict(posts)  # not dict, because of underlying hash map
+
 
 @app.route('/line')
 def line():
-    line_labels = labels
-    line_values = values
-    return render_template('line_chart.html', title='Bitcoin Monthly Price in USD', max=17000, labels=line_labels,
-                           values=line_values)
+    conn = get_db_connection()
+    db_data = conn.execute('SELECT * FROM temperature').fetchall()
+    conn.close()
+    db_data = OrderedDict(db_data)
+    line_dates = db_data.keys()
+    line_temperatures = db_data.values()
+    return render_template('line_chart.html', title='Temperature', labels=line_dates,
+                           values=line_temperatures)
 
 
 if __name__ == '__main__':
