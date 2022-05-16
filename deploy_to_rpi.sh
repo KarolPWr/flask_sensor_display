8f6a8069e5ca111d5d1cfe6bce7a464867d81498
Karol P <karol@przybylski>
2022-05-16 11:36:38 +0200

    Improved deploy script

--- deploy_to_rpi.sh
+++ deploy_to_rpi.sh
@@ -3,3 +3,8 @@
 #scp -r [!venv]* /home/karol.przybylski/PycharmProjects/flask_chart_test pi@192.168.0.163:/home/pi/flask_chart_test
 
 rsync -av --exclude 'venv' /home/karol.przybylski/PycharmProjects/flask_chart_test pi@192.168.0.163:/home/pi/
+
+ssh pi@192.168.0.163 << EOF
+  killall python
+  python /home/pi/flask_chart_test/app.py &
+EOF
