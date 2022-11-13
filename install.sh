5d0b722928fa097cb804d94ad652a784a2e25c3a
KarolPWr <karprzy7@gmail.com>
2022-11-13 18:41:49 +0100

    Added service for running the app at boot and in the background. Fixed timer not starting after reboot

--- install.sh
+++ install.sh
@@ -12,9 +12,12 @@ fi
 echo "Installing sensor_getter service and timer"
 cp "$SCRIPT_DIR"/sensor_specific/sensor_getter.service /etc/systemd/system
 cp "$SCRIPT_DIR"/sensor_specific/sensor_getter.timer /etc/systemd/system
+cp "$SCRIPT_DIR"/sensor_specific/app_runner.service /etc/systemd/system
 
 sed -i "/Type=oneshot/a ExecStart=python $SCRIPT_DIR/sensor_specific/sensor_getter.py" /etc/systemd/system/sensor_getter.service
 
 systemctl daemon-reload
 
 systemctl start sensor_getter.timer
+systemctl enable sensor_getter.timer
+systemctl enable app_runner.service
